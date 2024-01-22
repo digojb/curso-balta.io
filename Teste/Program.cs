d8604ac345215b15922dc3c2767eb3f3eecbca12
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-01-22 16:37:46 -0300

    First

---
+++ Teste/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Logitech", 200.00, EProductType.Product);
+
+            mouse.Id = 55;
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(EProductType.Product);
+        }
+    }
+
+    struct Product
+    {
+
+        public Product(int id, string name, double price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+    public int Id;
+    public string Name;
+    public double Price;
+    public EProductType Type;
+
+    public double PriceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
+
+enum EProductType
+{
+    Product = 1,
+    Service = 2
+}
+}
+
