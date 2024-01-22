2f80b0ef02b0dc57207b612cc3f84baff589dcbe
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-01-22 16:36:17 -0300

    First

---
+++ Inerpolacao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inerpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = 10.2;
+
+            //Primeira forma
+            //var texto = "O preço do produto é " + price;
+
+            //Segunda forma
+            //var texto = string.Format("O preço do produto é {0} apenas na promoção {1}", price, true);
+            /*Format é outra forma de adicionar o valor que desejamos, usamos as chaves e definimos o valor depois da virgula.
+            podemos adiconar varios valores nas chaves, cada um em uma posição.*/
+
+            //Terceira forma
+            var texto = $"O preço do produto é {price} apenas na promoção";
+
+            Console.WriteLine(texto);
+        }
+    }
+}
