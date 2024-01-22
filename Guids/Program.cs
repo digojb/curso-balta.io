4268561ed4508deb8d6994144d96aa79a6e4d083
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-01-22 16:35:49 -0300

    First

---
+++ Guids/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Guids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); //guid é um identificador global unico. Gera valores aleatorios.
+            id.ToString(); //transforma o id em string. o Console.WriteLine ja esta fazendo isso no caso.
+
+            id = new Guid("0f880d15-4f11-4b7a-9339-1f7451ea9139"); //usado para gerar um guid que ja existe. Caso esteja vazio será tudo zero.
+
+            Console.WriteLine(id.ToString().Substring(0, 8)); //ira pegar apenas os 8 primeiros numeros.
+        }
+    }
+}
