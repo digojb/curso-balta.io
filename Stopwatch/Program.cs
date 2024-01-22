9c11ccf33621363cd404f0e88ea68e0b7d5e622d
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-01-22 16:27:44 -0300

    First

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+        }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); //converte todos caracteres para minusculo.
+            char type = char.Parse(data.Substring(data.Length - 1, 1)); //pega o ultimo caractere que o usuario digitou, no caso, s ou m;
+            int time = int.Parse(data.Substring(0, data.Length - 1)); //pega apenas os numeros digitados.
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000); //pausa por 1 segundo e executa dnv.
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
